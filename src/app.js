2ef6dd60834bedccb3f3fa0327caf553695b1aca
ganeshbirajdar286 <ganeshbirajdar286@gmail.com>
2025-04-22 19:33:05 +0530

    multer

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 import express from  "express";
 import cors from "cors"
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 import cookieParser from "cookie-parser"
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const app =express()
+
 app.use(cors({
     origin:process.env.CORS_ORIGIN,
     credentials:true
@@ -10,7 +15,7 @@ app.use(express.json({limit:"16kb"})) // limits json to 16kbytes
 
  app.use(express.urlencoded({extended:true,limit:"16kb"}))//  base no url when we search google the url which goolge show https://www.google.com/search?gs_ssp=eJzj4tLP1TcoqiwoyE1XYDRgdGDwYkvPLMkoTQIAWC4HFA&q=github&oq=git&gs_lcrp=EgZjaHJvbWUqEggCEC4YJxjHARjRAxiABBiKBTIGCAAQRRg7Mg4IARBFGDsYQxiABBiKBTISCAIQLhgnGMcBGNEDGIAEGIoFMhQIAxBFGDkYQxiDARixAxiABBiKBTIPCAQQABhDGLEDGIAEGIoFMg8IBRAAGEMYsQMYgAQYigUyDwgGEAAYQxiLAxiABBiKBTIPCAcQABhDGIsDGIAEGIoFMhIICBAAGEMYiwMYsQMYgAQYigUyBwgJEAAYjwLSAQk0MDg4ajFqMTWoAgiwAgHxBekYGSOkwW5i&sourceid=chrome&ie=UTF-8
 
- app.use(express.static("public"))
+ app.use(express.static(path.join(__dirname,"public")))
  app.use(cookieParser())  //To read and manipulate cookies in Express applications.
- 
+  
 export {app}
