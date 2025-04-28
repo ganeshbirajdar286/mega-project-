fb851049dbc618ba25a131ebfe4af3b457927058
ganeshbirajdar286 <ganeshbirajdar286@gmail.com>
2025-04-28 13:33:39 +0530

    add new routes for user controller

--- src/routes/user.Routes.js
+++ src/routes/user.Routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logOutUser, registerUser } from "../controllers/user.controller.js";
+import { avatarUpdate, changeCurrentPassword, coverImageUpdate, getCurrentUser, getUserChannelProfile, getWatchHistory, loginUser, logOutUser, registerUser, updatedUserDetails } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import{ VerifyJWT} from "../middlewares/auth.middleware.js"
 import {refreshAccessToken} from "../controllers/user.controller.js"
@@ -24,4 +24,13 @@ loginUser
 
 router.route('/logout').post(VerifyJWT,logOutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(VerifyJWT,changeCurrentPassword)
+router.route("/current-user").post(VerifyJWT,getCurrentUser)
+router.route("/updated-account").patch(VerifyJWT,updatedUserDetails) // update is taken place that why we use patch instayed of post.if we use post it update wholt model
+router.route("/avatar").patch(VerifyJWT,upload.single("avatar"),avatarUpdate)
+router.route("/cover-image").patch(VerifyJWT,upload.single("coverImage"),coverImageUpdate)
+router.route("/c/:username").get(VerifyJWT, getUserChannelProfile)
+router.route("/history").get(VerifyJWT, getWatchHistory)
+
+
 export default router 
