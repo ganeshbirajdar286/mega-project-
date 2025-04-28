396b6d82a0998125fc62563982ad6b0f7dc28c22
ganeshbirajdar286 <ganeshbirajdar286@gmail.com>
2025-04-28 13:12:33 +0530

    add new feature for watchHistory

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,6 +5,7 @@ import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/APIresponse.js";
 import fs from "fs"
 import jwt from "jsonwebtoken"
+import mongoose from "mongoose";
 
 
 const generateAccessAndRefereshTokens= async(userId)=>{
@@ -372,6 +373,60 @@ return res
    new ApiResponse(200,channel[0],"user channel fetched successfully")
 )
 })
+//  aggregation pipelines  from watchHistory
+const getWatchHistory = asyncHandler(async(req,res)=>{
+    const user =await User.aggregate([
+      {
+         $match:{
+            _id:mongoose.Types.ObjectId(req.user._id)  // req.user._id return string not mongodb id this string goto mongoose and mongoose tell monogobd it's id.  but when aggregation take place this code directly go to mongodb not to mongoose.so that why we have to use  "mongoose.Types.ObjectId(req.user._id)" .this tell  mongodb that this is mongodb id
+         }
+      },
+      {
+         $lookup:{
+            from:"videos",
+            localField:"watchHistory",
+            foreignField:"_id",
+            as:"watchHistory",
+            pipeline:[
+               {
+                  $lookup:{
+                     from:"users",
+                     localField:"owner",
+                     foreignField:"_id",
+                     as:"owner"
+                  },
+                  pipeline:[
+                     {
+                        $project:{
+                           fullName:1,
+                           username:1,
+                           avatar:1,
+                        }
+                     }
+                  ]
+               },
+               {
+                  $addFields:{
+                       owner:{
+                        $arrayElemAt:["$owner",0]
+                       }
+                  }
+               }
+            ]
+         }
+      }
+
+
+    ])
+    return res
+    .status(200)
+    .json(
+      new ApiResponse(200,
+         user[0].watchHistory,
+         "watch history fecthed  for user"
+      )
+    )
+})
 export {registerUser,
       loginUser,
       logOutUser,
@@ -381,5 +436,6 @@ export {registerUser,
       updatedUserDetails,
       avatarUpdate,
       coverImageUpdate,
-      getUserChannelProfile
+      getUserChannelProfile,
+      getWatchHistory
    }
